6d11ad7ec79711e291a3eb318e18b5c3842e70a1
Leonel <leonelaf12@gmail.com>
2019-05-29 17:16:58 -0400

    made post method for posts

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,25 +1,76 @@
-const express = 'express';
+const express = require('express');
+
+const Users = require('./userDb.js');
+const Posts = require('../posts/postDb.js');
 
 const router = express.Router();
 
-router.post('/', (req, res) => {
-
+router.post('/', validateUser, async (req, res) => {
+   try {
+       const newAccount = await Users.insert(req.body);
+       res.status(201).json(newAccount);
+   } catch (error){
+    console.log(error);
+    res.status(500).json({
+        message: "Error adding the user"
+    });
+   }
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.post('/:id/posts', validatePost, async (req, res) => {
+    const postInfo = { ...req.body, user_id: req.params.id };
+    try {
+        const post = await Posts.insert(postInfo);
+        res.status(201).json(post);
+    } catch(error){
+        console.log(error);
+        res.status(500).json({
+            message: "Error adding post"
+        });
+    }
 });
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const accounts = await Users.get(req.query);
+        res.status(200).json(accounts);
+    } catch (error){
+        console.log(error);
+        res.status(500).json({
+            message: "Error retrieving accounts"
+        });
+    }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        const account = await Users.getById(req.params.id);
+        
+        if(account){
+            res.status(200).json(account);
+        } else {
+            res.status(404).json({
+                message: "User not found"
+            })
+        }
+    } catch (error){
+        console.log(error);
+        res.status(500).json({
+            message: "Error retrieving user"
+        });
+    }
 });
 
-router.get('/:id/posts', (req, res) => {
-
+router.get('/:id/posts', async (req, res) => {
+    try {
+    const posts = await Posts.getById(req.params.id)
+    res.status(200).json(posts);
+    } catch (error){
+        console.log(error);
+        res.status(500).json({
+            message: "Error retrieving posts"
+        });
+    }
 });
 
 router.delete('/:id', (req, res) => {
