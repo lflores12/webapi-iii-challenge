d68637414bb2dc75651b9ea61531d5727a259767
Leonel <leonelaf12@gmail.com>
2019-05-29 18:51:41 -0400

    was able to do stretch and have everything functional

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,74 @@
-const express = 'express';
+const express = require('express');
+
+const Posts = require('./postDb');
+
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {
+        const posts = await Posts.get(req.query);
+        res.status(200).json(posts);
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Error retrieving posts"
+        });
+    }
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', async (req, res) => {
+    try {
+        const post = await Posts.getById(req.params.id);
+        
+        if(post){
+            res.status(200).json(post);
+        } else {
+            res.status(404).json({
+                message: "Post not found"
+            })
+        }
+    } catch(error){
+        console.log(error);
+        res.status(500).json({
+            message: "Error retrieving Post"
+        });
+    }
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', validatePostId, async (req, res) => {
+    try {
+        res.status(200).json( await Posts.remove(req.params.id));
+    } catch(error) {
+        console.log(error);
+        res.status(500).json({
+            message: "Error removing that post"
+        })
+    }
 });
 
-router.put('/:id', (req, res) => {
-
+router.put('/:id', validatePostId, async (req, res) => {
+    try {
+        res.status(200).json( await Posts.update(req.params.id, req.body));
+    } catch(error){
+        console.log(error);
+        res.status(500).json({
+            message: "error updating user"
+        })
+    }
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-
+    const id = `${req.params.id}`;
+    if(!id){
+        res.status(400).json({
+            message: "invalid post id" 
+        })
+    } else {
+        req.post = id;
+    } next();
 };
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const server = express();
 
-const userRouter = require('./users/userRouter.js')
+const userRouter = require('./users/userRouter.js');
+const postRouter = require('./posts/postRouter.js');
 
 server.use(express.json());
 server.use(logger);
 
 server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
